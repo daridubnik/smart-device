790b4d1a58f6492aff64def0ca5cee6f034d8aea
Daria Dubnikova <daridubnik@gmail.com>
2023-03-16 00:02:30 +0300

    Подключает аккордеон в футере

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {initAccordions} from './accordions/init-accordion';
 
 // ---------------------------------
 
@@ -21,6 +22,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+  });
+});
+
+// Accordions
+// ---------------------------------
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initAccordions();
   });
 });
 
