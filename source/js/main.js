610a72ee4df40b3369a7392d62968857b9b309ca
Daria Dubnikova <daridubnik@gmail.com>
2023-03-20 02:55:49 +0300

    Дорабатывает проект по критериям

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
-import {initAccordions} from './accordions/init-accordion';
+import {initAccordions} from './modules/accordions/init-accordion';
 
 // ---------------------------------
 
